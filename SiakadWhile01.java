19199555c73a0952175d7edbc765194a4e56d868
Adi Luhung <luhungadi571@gmail.com>
2024-10-11 10:08:53 +0700

    Modifikasi

--- SiakadWhile01.java
+++ SiakadWhile01.java
@@ -19,6 +19,7 @@ public class SiakadWhile01 {
 
             if (nilai > 80 && nilai < 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah A");
+                System.err.println("Bagus, pertahankan nilainya!");
             } else if (nilai > 73 && nilai <= 80) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah B+");
             } else if (nilai > 65 && nilai <= 73) {
@@ -32,6 +33,7 @@ public class SiakadWhile01 {
             } else {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + " adalah E");
             }
+
             i++;
         } sc.close();
     }
